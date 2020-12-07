c59ee4a11e2d049898225b7cef8f66b5a0bebddd
Rachit <r.lohani555@gmail.com>
2020-12-07 20:53:48 +0530

    date test

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Dec 5, 2020',
+  birthdate: 'Dec 8, 2020',
   name: 'Ara'
 }; 
 
