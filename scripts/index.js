e290cc1956b2fcafb2897db43a6ec82c88c20865
Rachit <r.lohani555@gmail.com>
2020-12-10 13:43:39 +0530

    date correction

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Dec 8, 2020',
+  birthdate: 'Dec 17, 2020',
   name: 'Ara'
 }; 
 
